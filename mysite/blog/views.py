1ac1ac99b025510920b8074c890237a4ec1945f7
Filip-Gluszcz <80832642+Filip-Gluszcz@users.noreply.github.com>
2022-08-01 13:25:41 +0200

    posts list and detail pages / detail view fix
    There was no return in the post detail view...

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,18 +1,22 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 
+                    'blog/post/list.html', 
+                    {'posts': posts})
 
 def post_detail(request, year, month, day, post):
-    post = get_list_or_404(Post, slug=post, 
-                                status='published', 
-                                publish__year=year, 
-                                publish__month=month, 
-                                publish__day=day)
+    post = get_object_or_404(Post, slug=post, 
+                                   status='published', 
+                                   publish__year=year, 
+                                   publish__month=month, 
+                                   publish__day=day)
                             
-    render(request, 'blog/post/detail.html', {'post': post})
+    return render(request, 
+                    'blog/post/detail.html', 
+                    {'post': post})
 
 
 
